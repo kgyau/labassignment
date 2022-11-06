7cb5aefa3c18cd5f788c85792430189bf0e2cfe3
kwame.ofori-gyau@ashesi.edu.gh <kwame.ofori-gyau@ashesi.edu.gh>
2022-11-06 20:57:23 +0000

    lab4

---
+++ actions/add_product.php
@@ -0,0 +1,43 @@
+<?php
+//making actuioin aware fof controller
+include("../controllers/product_controller.php");
+
+
+
+// collecting form data
+if(isset($_POST['submitprod'])){
+    // for image 
+    // var_dump($pic);
+ // I AM INTERESTED INN THE TWO OTHE RITEMS AND PUTTIGN THEM IN A VARIABLE
+    $prodcat =$_POST['prodcat'];
+    $prodbrand =$_POST['prodbrand'];
+    $prodtitle =$_POST['prodtitle']; 
+    $prodprice =$_POST['prodprice'];
+    $proddesc =$_POST['proddesc'];
+    $prodkeywords=$_POST['prodkeywords'];
+    $prodimage = NULL;
+    
+    $pic= "../images/product/".basename($_FILES['prodimage']['name']);
+
+
+//    echo $prodcat,$prodtitle,$prodprice,$prodbrand,$proddesc,$prodimage,$prodkeywords; 
+
+
+  $result = addproduct_ctr($prodcat,$prodbrand,$prodtitle,$prodprice,$proddesc,$prodimage,$prodkeywords);
+
+    if ($result) {
+
+        if (move_uploaded_file($_FILES['prodimage']['tmp_name'], $pic)) {
+            echo "success";
+    
+        }else {
+            echo "fail";
+        }
+        header("location:../admin/product.php");
+
+    } else{
+         header("location:../admin/product.php");
+    }
+}
+
+?>
