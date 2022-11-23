10f5804a890429753f1e59d43caf802cbf6abebc
Kwame Ofori-Gyau <112716883+kgyau@users.noreply.github.com>
2022-11-23 11:56:54 +0000

    Update updateprod_process.php
    zjhz

--- actions/updateprod_process.php
+++ actions/updateprod_process.php
@@ -9,29 +9,28 @@ if(isset($_POST['Updatep'])){
    $productprice=$_POST['product_price'];
    $prod_desc=$_POST['product_desc'];
    $prod_key=$_POST['product_keywords'];
+//    $productimage=$_POST['prodimage'];
 
-   // $productimage=$_POST['prodimage'];
 
 
-   
+  
   $prodimage =$_FILES['prodimage']['name'];
    $targetdir= "../images/product/";
    $image = $targetdir . $prodimage;
-   $file = '../images/product/' .basename($_FILES["prodimage"]["name"]);
+   $file = "../images/product/" .basename($_FILES["prodimage"]["name"]);
    move_uploaded_file($_FILES["prodimage"]["tmp_name"],$file); 
 
   
-  
+//    $result= editprod_ctr($prod_id,$productcat,$productbrand,$prod_title,$productprice,$prod_desc,$prod_key,$file);
+   echo $prod_id,$productcat,$productbrand,$prod_title,$productprice,$prod_desc,$prod_key,$file;
 
-   $result= editprod_ctr($prod_id,$productcat,$productbrand,$prod_title,$productprice,$prod_desc,$prod_key,$file);
+//    if($result==True){
+//     header("location:../view/all_product.php");
+//    }
 
-   if($result==True){
-    header("location:../view/all_product.php");
-   }
-
-   else{
-    echo "Could not update";
-   }
+//    else{
+//     echo "Could not update";
+//    }
 }
 ?>
 
